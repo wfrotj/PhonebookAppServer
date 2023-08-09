a93745dada1b52cbe24f6fdcf2b3697b3fd873c1
wfrotj <trebrodrigo@gmail.com>
2023-08-09 11:09:26 +0800

    edit dist

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import app from "./app.js";
 import config from "./utils/config.js";
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/login", (_req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(config.PORT, () => {
   console.log(`Server is running on port: ${config.PORT}`);
